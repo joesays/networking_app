e55785410d94dd7bf6e9e3f4ffc810ef98c2f56f
Raghu Betina <raghu@firstdraft.com>
2019-06-06 21:39:15 +0000

    Direct association between locations and connections

--- app/models/connection.rb
+++ app/models/connection.rb
@@ -1,6 +1,8 @@
 class Connection < ApplicationRecord
   # Direct associations
 
+  belongs_to :location
+
   belongs_to :company
 
   has_many   :attendees,
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,6 +1,9 @@
 class Location < ApplicationRecord
   # Direct associations
 
+  has_many   :connections,
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
