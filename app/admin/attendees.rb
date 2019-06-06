2107068bb81c544dc908097c2fc42092691e8b49
Raghu Betina <raghu@firstdraft.com>
2019-06-06 21:39:55 +0000

    Whitelist columns in Attendee for ActiveAdmin

--- app/admin/attendees.rb
+++ app/admin/attendees.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Attendee do
+
+ permit_params :meeting_id, :connection_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
