f8c3ad8e83356b9343e2631b229e88329816cdff
Raghu Betina <raghu@firstdraft.com>
2019-06-06 21:39:42 +0000

    Whitelist columns in Connection for ActiveAdmin

--- app/admin/connections.rb
+++ app/admin/connections.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Connection do
+
+ permit_params :company_id, :location_id, :photo, :name, :linkein
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
