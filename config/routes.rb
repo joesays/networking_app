644c1d69e22893dada9988b90f04b32e015e6393
Raghu Betina <raghu@firstdraft.com>
2019-06-06 21:39:04 +0000

    Generate companies resource with draft:resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,23 @@
 Rails.application.routes.draw do
+  # Routes for the Company resource:
+
+  # CREATE
+  get("/companies/new", { :controller => "companies", :action => "new_form" })
+  post("/create_company", { :controller => "companies", :action => "create_row" })
+
+  # READ
+  get("/companies", { :controller => "companies", :action => "index" })
+  get("/companies/:id_to_display", { :controller => "companies", :action => "show" })
+
+  # UPDATE
+  get("/companies/:prefill_with_id/edit", { :controller => "companies", :action => "edit_form" })
+  post("/update_company/:id_to_modify", { :controller => "companies", :action => "update_row" })
+
+  # DELETE
+  get("/delete_company/:id_to_remove", { :controller => "companies", :action => "destroy_row" })
+
+  #------------------------------
+
   # Routes for the Connection resource:
 
   # CREATE
