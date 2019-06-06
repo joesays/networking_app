b341695c1bb9c95ee14284554121601c0abb9df6
Raghu Betina <raghu@firstdraft.com>
2019-06-06 21:40:35 +0000

    Only meeting user can modify meeting

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,4 +1,14 @@
 class MeetingsController < ApplicationController
+  before_action :current_user_must_be_meeting_user, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_user_must_be_meeting_user
+    meeting = Meeting.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_user == meeting.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @meetings = Meeting.all
 
