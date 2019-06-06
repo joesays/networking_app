3e3a14a5a01456032931332ffe8a1ad8461b2752
Raghu Betina <raghu@firstdraft.com>
2019-06-06 21:39:27 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "meetings#index"
   # Routes for the Attendee resource:
 
